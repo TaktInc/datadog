aa8e4c0d372913ce18b2fee1d3ca722ed9a8d83f
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2017-12-19 00:13:30 -0800

    Add ps file to show how it looks like

--- leak.sh
+++ leak.sh
@@ -4,8 +4,9 @@
 
 set -euo pipefail
 
+rm -f *.ps *.hp
 stack build --profile --ghc-options="-rtsopts"
 stack install
-./bin/datadog-leak 10000 +RTS -L32 -hm
+./bin/datadog-leak 1000 +RTS -L32 -hm
 stack exec -- hp2ps datadog-leak.hp
 open datadog-leak.ps
